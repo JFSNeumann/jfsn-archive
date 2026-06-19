39f07eb86535dc2edcb0158ddadeba4b9f08af36
JFSNeumann <Jfsneumann@gmail.com>
2026-06-19 13:05:22 -0400

    Update cache version for Session 72 final deployment

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
    To invalidate all caches: bump CACHE_V below, then deploy. */
 
-const CACHE_V  = 'jfsn-1781874578'; // Session 70: Add section tinting + load animations to 9 pages
+const CACHE_V  = 'jfsn-1781888523'; // Session 72 COMPLETE: ALL archive shadows removed from all selectors
 const PRECACHE = [
   '/',
   '/index.html',
